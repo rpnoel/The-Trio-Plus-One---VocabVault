f6702125d68f5be86a4a99b26d0bd11a47569e08
USCIlgenfritz <143644267+USCIlgenfritz@users.noreply.github.com>
2024-10-06 19:28:35 -0400

    Created Rank.java

---
+++ Rank.java
@@ -0,0 +1,20 @@
+public class Rank{
+
+  public Rank(LeagueType type, int ranking){
+  }
+
+  public int getRank(){
+    return ranking
+  }
+
+  public void setRank(int rank){
+  }
+
+  public League getLeague(){
+    return type;
+  }
+
+  public void setLeague(LeagueType type){
+  }
+  
+}
