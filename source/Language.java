b2e3d5e1ea0b53e0755c67f7d8e3e304d8fe76f7
Max Granger <zoegranger2005@gmail.com>
2024-10-07 08:54:55 -0400

    Added Language.java

--- source/Language.java
+++ source/Language.java
@@ -1,3 +1,15 @@
+//@author Max Granger
+
 public enum Language {
-    
+    SPANISH("Spanish"),
+    FRENCH("French"),
+    GERMAN("German"),
+    RUSSIAN("Russian"),
+    JAPANESE("Japanese");
+
+    public final String label;
+
+    private Language(String label) {
+        this.label = label;
+    } 
 }
