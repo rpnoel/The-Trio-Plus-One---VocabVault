cdd5f55d71f362ecb80af4775c6ee87620ff67e0
USCIlgenfritz <143644267+USCIlgenfritz@users.noreply.github.com>
2024-10-13 14:29:33 -0400

    Update Question.java

--- VocabVault/source/Question.java
+++ VocabVault/source/Question.java
@@ -5,14 +5,22 @@ public class Question {
     private QuestionType questionType;
 
     public boolean checkAnswer(String answer){
-        
+        if (answer.equals(correctAnswer){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setCorrectAnswer(String correctAnswer){
+        this.correctAnswer = correctAnswer;
     }
 
     public ArrayList<String> getChoices(){
-
+        return choices;
     }
 
     public String getCorrectAnswer(){
-        
+        return correctAnswer;
     }
 }
