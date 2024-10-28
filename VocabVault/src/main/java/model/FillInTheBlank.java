fe5b16aacece8308492d67cd5d6f7d838f39733a
Max Granger <zoegranger2005@gmail.com>
2024-10-28 11:30:03 -0400

    Update 10/28

--- VocabVault/src/main/java/model/FillInTheBlank.java
+++ VocabVault/src/main/java/model/FillInTheBlank.java
@@ -4,6 +4,7 @@ package model;
 public class FillInTheBlank extends Question {
     public String questionText;
     public String correctAnswer;
+    public String userAnswer;
     
     public FillInTheBlank(Word word) {
         this.questionText = "Translate the following to Spanish: " + word.getTranslation();
